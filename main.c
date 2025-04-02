9baee4251f6bd093541bde82525100a23a8ab605
Zechariah Kim <zechandezra123@gmail.com>
2025-04-01 21:54:08 -0700

    Added socket binding functionality and fixed headers

--- main.c
+++ main.c
@@ -1,23 +1,38 @@
 #include <stdio.h>
 #include <sys/socket.h>
 #include <string.h>
-#include <cstdint>
+#include <stdint.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 
-struct sockaddr_in {
-    uint16_t       sin_family; // AF_INET
-    uint16_t       sin_port;   // port in big-endian
-    struct in_addr sin_addr;   // IPv4
-};
-struct in_addr {
-    uint32_t       s_addr;     // IPv4 in big-endian
-};
+void die(const char *msg) {
+    perror(msg);
+    exit(1);
+}
 
 int main() {
     printf("Hello, world!\n");
+
+    // Initialize a socket and bind to fd handle
     int fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (fd < 0) { die("socket()"); }
     printf("file descriptor: %d\n", fd);
+    
     int val = 1;
-    setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &val, sizeof(val));
+    if (setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &val, sizeof(val)) < 0) {
+        die("setsockopt()");
+    }
+
     struct sockaddr_in addr = {};
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(1234);
+    addr.sin_addr.s_addr = htonl(0);
+
+    int rv = bind(fd, (const struct sockaddr *) &addr, sizeof(addr));
+    if (rv) { die("bind()"); }
+
+    printf("Successfully bound to port 1234\n");
     return 0;
 }
